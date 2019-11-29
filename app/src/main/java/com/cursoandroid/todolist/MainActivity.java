f5cceed9aaab4309a224aad5a150ec6e22072810
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-29 16:43:12 -0300

    Implementing save method.

--- app/src/main/java/com/cursoandroid/todolist/MainActivity.java
+++ app/src/main/java/com/cursoandroid/todolist/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,18 +39,18 @@ public class MainActivity extends AppCompatActivity {
             banco = openOrCreateDatabase("appTarefas", MODE_PRIVATE, null);
 
             //Creating table
-            banco.execSQL("CREATE TABLE IF NOT EXISTS tarefas(id INTERGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
+            banco.execSQL("CREATE TABLE IF NOT EXISTS tarefas(id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
 
             btnAdicionar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     entradaTarefa = txtTarefa.getText().toString();
-                    banco.execSQL("INSERT INTO tarefas (tarefa) VALUES('" + entradaTarefa + "')");
+                    salvarTarefa(entradaTarefa);
                 }
             });
 
             //get cursor
-            cursor = banco.rawQuery("SELECT * FROM taferas", null);
+            cursor = banco.rawQuery("SELECT * FROM tarefas", null);
 
             //get columns index
             indiceColunaId = cursor.getColumnIndex("id");
@@ -57,7 +59,27 @@ public class MainActivity extends AppCompatActivity {
             //set cursor to first element
             cursor.moveToFirst();
 
+            while (cursor != null){
+                Log.i("RESULTADO - ", "ID [" + cursor.getString(indiceColunaId) + "] " + cursor.getString(indiceColunaTarefa));
+                cursor.moveToLast();
+            }
+
         } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void salvarTarefa(String tarefa){
+        try {
+            if (!tarefa.equals("")){
+                banco.execSQL("INSERT INTO tarefas (tarefa) VALUES('" + tarefa + "')");
+                Toast.makeText(MainActivity.this, R.string.tarefaSalva, Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, R.string.tarefaVazia, Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
