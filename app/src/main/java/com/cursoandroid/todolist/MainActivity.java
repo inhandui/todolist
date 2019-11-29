9b1ecb8d83c190d04103279f7c644c0fb6d231bc
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-29 15:53:48 -0300

    Get interface elements reference.

--- app/src/main/java/com/cursoandroid/todolist/MainActivity.java
+++ app/src/main/java/com/cursoandroid/todolist/MainActivity.java
@@ -3,12 +3,24 @@ package com.cursoandroid.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtTarefa;
+    private Button btnAdicionar;
+    private ListView lvTarefas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTarefa = findViewById(R.id.txtTarefa);
+        btnAdicionar = findViewById(R.id.btnAdicionar);
+        lvTarefas = findViewById(R.id.lvTarefas);
     }
 }
